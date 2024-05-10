5d98549bd23d9589add035afed3372d00fe59483
dharam-in <dharamdotin@gmail.com>
2024-05-10 08:53:37 +0530

    Navbar Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h2>India</h2>
+    <main className="min-h-screen bg-black/[0.9] antialiased bg-grid-white/[0.02]">
+      <h2 className="text-2xl text-center">India</h2>
+    </main>
   );
 }
