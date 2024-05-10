fce827687d581c7904ae527223c46ce6c717808a
dharam-in <dharamdotin@gmail.com>
2024-05-10 08:20:30 +0530

    Next App Setup

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { useState } from 'react'
+import { HoveredLink, Menu, ProductItem, MenuItem } from './ui/navbar-menu'
+import { cn } from '@/utils/cs'
+import Link from 'next/link'
+
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar
