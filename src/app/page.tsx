38ab4fc052ca6362f254ddd4717a1941ddf7faef
dharam-in <dharamdotin@gmail.com>
2024-05-11 17:54:20 +0530

    Sticky Scroll Add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeatureCourses from "@/components/FeatureCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChoose from "@/components/WhyChoose";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.9] antialiased bg-grid-white/[0.02]">
       <HeroSection/>
       <FeatureCourses/>
+      <WhyChoose/>
     </main>
   );
 }
